1edeec3c5baf0bead97698a4dbfcf96aa47ade33
DenysSidorov <000scorpions000@gmail.com>
2016-11-09 16:51:19 +0200

    parse news

--- main.js
+++ main.js
@@ -8,26 +8,7 @@ request('https://news.ycombinator.com', function (error, response, html) {
         var $ = cheerio.load(html);
         $('span.comhead').each(function(i, element){
             var a = $(this).prev();
-            var rank = a.parent().parent().text();
-            var title = a.text();
-            var url = a.attr('href');
-            var subtext = a.parent().parent().next().children('.subtext').children();
-            var points = $(subtext).eq(0).text();
-            var username = $(subtext).eq(1).text();
-            var comments = $(subtext).eq(2).text();
-            // Our parsed meta data object
-            var metadata = {
-                rank: parseInt(rank),
-                title: title,
-                url: url,
-                points: parseInt(points),
-                username: username,
-                comments: parseInt(comments)
-            };
-            console.log(metadata);
+            console.log(a.text());
         });
     }
 });
-
-
-
