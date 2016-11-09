cd7be0fc0367f0065b64ada2c4e8035b49454e6e
DenysSidorov <000scorpions000@gmail.com>
2016-11-09 16:50:05 +0200

    parse news

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 /**
  * Created by Denis on 09.11.2016.
  */
-
+var cheerio = require('cheerio');
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
+request('https://news.ycombinator.com', function (error, response, html) {
     if (!error && response.statusCode == 200) {
-        console.log(body); // Show the HTML for the Google homepage.
+        var $ = cheerio.load(html);
+        $('span.comhead').each(function(i, element){
+            var a = $(this).prev();
+            var rank = a.parent().parent().text();
+            var title = a.text();
+            var url = a.attr('href');
+            var subtext = a.parent().parent().next().children('.subtext').children();
+            var points = $(subtext).eq(0).text();
+            var username = $(subtext).eq(1).text();
+            var comments = $(subtext).eq(2).text();
+            // Our parsed meta data object
+            var metadata = {
+                rank: parseInt(rank),
+                title: title,
+                url: url,
+                points: parseInt(points),
+                username: username,
+                comments: parseInt(comments)
+            };
+            console.log(metadata);
+        });
     }
 });
 
-var cheerio = require('cheerio');
-var $ = cheerio.load('<h2 class="title">Hello world</h2>');
 
-$('h2.title').text('Hello there!');
-$('h2').addClass('welcome');
 
-$.html();
-//=> <h2 class="title welcome">Hello there!</h2>
